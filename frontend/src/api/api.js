fb7f8ca6077f5b32e0546e85ec80741a7e2de8f1
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-08-16 19:31:38 +0530

    api for fetching and sending data

---
+++ frontend/src/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:8000/v1/reco";
+
+export const getHomefeed = async (userId) => {
+  const res = await fetch(`${API_URL}/homefeed?user_id=${userId}`, {
+    method: 'GET',
+    headers: {"Content-Type": "application/json"}
+  });
+  console.log(res.json)
+  return res.json();
+};
+
+export const sendFeedback = async ({ user_id, item_id, feedback_type }) => {
+  const res = await fetch(`${API_URL}/feedback`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user_id, item_id, feedback_type }),
+  });
+  return res.json();
+};
+
+export const getExplanations = async (user_id, top_k=10) => {
+  const res = await axios.get(`${API_URL}/explanations`, { params: { user_id, top_k } });
+  console.log(res.data)
+  return res.data;
+};
